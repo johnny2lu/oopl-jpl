db04cc31caab4b8c4dd8f96db083010e4ca9b0d6
Glenn Downing <downing@cs.utexas.edu>
2018-01-10 10:26:50 -0800

    another commit

--- notes/01-10.py
+++ notes/01-10.py
@@ -3,4 +3,87 @@
 # -----------
 
 """
+assertions are good for preconditions and postconditions
+they're not good for user errors, instead exceptions
+they're not good for testing, instead unittest
+
+tests are only as good as the coverage that they achieve
+
+exceptions
+try, except, else, finally
+what was the concern with except clause that are trying to handle types in a class hierarchy
 """
+
+x = 2         # int, a value
+x = [2, 3, 4] # list, an address
+
+# simple types
+int
+bool
+float
+
+# composite types
+list  # implemented with a front-loaded array
+tuple # like list, but immutable
+set   # like list, but no duplicates and order doesn't matter
+dict  # list set,  but it has keys AND values
+deque # doubly-linked list, very different complexities to an array
+
+print([2, 3, 4] is [2, 3, 4]) # false
+print([2, 3, 4] == [2, 3, 4]) # true
+print([2, 3, 4] == [2, 4, 3]) # false
+
+print({2, 3, 4} is {2, 3, 4}) # false
+print({2, 3, 4} == {2, 3, 4}) # true
+print({2, 3, 4} == {2, 4, 3}) # true
+
+# operators
+# + vs +=
+# expressions vs statement
+
+x = ()
+
+# list's += is okay with any iterable on the right-hand-side
+
+# tuple's += is NOT okay with any iterable on the right-hand-side
+# it must be another tuple
+
+x += y
+
+# is that the same as (yes, for immutables and no for mutable)
+
+x = x + y
+
+x + ?
+
+# list's + is NOT okay with any iterable on the right-hand-side
+# it must be another list
+
+# tuple's + is NOT okay with any iterable on the right-hand-side
+# it must be another tuple
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
